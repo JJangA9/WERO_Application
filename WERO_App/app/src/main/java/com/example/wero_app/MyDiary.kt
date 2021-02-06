ccfa79a481e26e18e419814561defde944e872b1
xeaquz <44634582+xeaquz@users.noreply.github.com>
2021-02-06 21:19:33 +0900

    floating button + write diary

--- WERO_App/app/src/main/java/com/example/wero_app/MyDiary.kt
+++ WERO_App/app/src/main/java/com/example/wero_app/MyDiary.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class MyDiary : Fragment() {
@@ -11,5 +14,22 @@ class MyDiary : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View = inflater.inflate(R.layout.my_diary,container,false)
+    ): View? {
+        val view = inflater.inflate(R.layout.my_diary,container,false)
+        val items = mutableListOf<ListViewItem>()
+        val listView = view.findViewById<ListView>(R.id.list_view)
+        items.add(ListViewItem("21.02.06", "간다ㅏ라마바사아자차캌타파하"))
+        items.add(ListViewItem("21.02.04", "이잉 ㅡㅠ "))
+
+        val adapter = ListViewAdapter(items)
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener { parent: AdapterView<*>, view: View, position: Int, id: Long ->
+            val item = parent.getItemAtPosition(position) as ListViewItem
+        }
+
+        return view
+    }
+
+
 }
